e9185d14e18c86fdbbb182515461c7e11073f5d1
drakegawain <thales.souzalopes@gmail.com>
2023-12-27 17:09:53 -0300

    dnsadna

--- main.py
+++ main.py
@@ -12,13 +12,36 @@ def main():
     zeta = 0.026
     wn = math.sqrt(k/m)
 
-    G, wc = calc_cartesian(N, wn, zeta)
+    real_part, wc = calc_cartesian(N, wn, zeta)
 
-    plt.plot(wc, G)
+    plt.plot(wc, real_part)
     plt.ylabel("Real Part")
+    plt.xlabel("frequency [Hz]")
     plt.show()
 
     cwc = 650
+    
+    g = G(wn, cwc, zeta)
+    h = H(wn, cwc, zeta)
+    fi = phi(g,h)
+    a_l = a_lim(kf, g)
+    T = T_vector(cwc, wn, zeta, 10)
+    V = velocity_vector(T)
+    print("wn: {}".format(wn))
+    print("phi: {}".format(fi))
+    print("alim: {}".format(a_l))
+    print("T: {}".format(T))
+    print("N: {}".format(V))
+    NV, alim = a_lim_vector(kf, wn, zeta, T)
+
+    print("NV: {}".format(NV))
+    print("alim: {}".format(alim))
+
+    plt.plot(NV, alim)
+    plt.ylabel("A_lim[mm]")
+    plt.xlabel("N[rev/min]")
+    plt.plot()
+
 
     return
 
@@ -66,17 +89,18 @@ def calc_cartesian(wc_range, wn, zeta):
 
 def phi(G, H):
 
-    res = math.degrees(math.atan(H/G))
+    res = math.atan(H/G)
 
     return res
 
 
-def velocity_vector(T):
+def velocity_vector(T:list):
 
     res = []
-    for t in range(T):
+    for t in range(len(T)):
 
-        transient = 60/t
+
+        transient = 60/T[t]
         res.append(transient)
 
 
@@ -84,20 +108,49 @@ def velocity_vector(T):
 
 
 
-def T_vector(wc, wn, zeta, n, phi):
+def T_vector(wc, wn, zeta, n):
 
     pi = math.pi
-    G = G(wn, wc, zeta)
-    H = H(wn, wc, zeta)
-    phi = phi(G, H)
+    g = G(wn, wc, zeta)
+    h = H(wn, wc, zeta)
+    
+    fi = phi(g, h)
     response = []
 
     for number in range(n):
-        transient = (2*number*pi)+(3*pi)+(2*phi)
+        transient = (2*number*pi)+(3*pi)+(2*fi)
         transient = transient/wc
         response.append(transient)
 
     return response
+
+
+def a_lim(kf, g):
+
+    transient = 2*kf*g
+
+    return -1/transient
+
+
+def a_lim_vector(kf, wn, zeta, T_vector_calc):
+
+    w = []
+    g_vec = []
+    a_lim_vec = []
+
+    for t in T_vector_calc:
+        w.append((1/t)*2*math.pi)
+
+    for n in range(len(w)):
+        g_vec.append(G(wn, w[n], zeta))
+
+    for g in g_vec:
+        a_lim_vec.append(a_lim(kf, g))
+
+    vel = velocity_vector(T_vector_calc)
+
+    return vel, a_lim_vec 
+
 
 
 if __name__ == "__main__":
